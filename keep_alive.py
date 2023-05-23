3fdccf4e63ce3e37950e087c7dfe16a3ff4c02b7
James Medley <81381425+jmed13@users.noreply.github.com>
2023-05-23 20:53:00 +0100

    api endpoints

--- keep_alive.py
+++ keep_alive.py
@@ -1,18 +1,41 @@
 from flask import Flask
 from threading import Thread
+import json
 
 app = Flask(' ')
 
-
 @app.route('/')
 def home():
-    return "I'm alive"
+    return """
+    <h1>Leaderboard bot</h1>
+    <ul>
+        <li><a href="/wakingup">Waking Up Award</a></li>
+        <li><a href="/lastmessage">Last Message Of The Day</a></li>
+        <li><a href="/streaks">Streaks</a></li>
+    </ul>
+    """
 
+@app.route('/wakingup')
+def wakingup():
+  with open("WUscores.json", "r") as f:
+    data = json.load(f)
+  return data
+
+@app.route('/lastmessage')
+def lastmessage():
+  with open("LMscores.json", "r") as f:
+    data = json.load(f)
+  return data
+
+@app.route('/streaks')
+def streaks():
+  with open("streak.json", "r") as f:
+    data = json.load(f)
+  return data
 
 def run():
-    app.run(host="0.0.0.0", port=5000)
-
+  app.run(host = "0.0.0.0", port = 8081)
 
 def keep_alive():
-    t = Thread(target=run)
-    t.start()
+  t= Thread(target=run)
+  t.start()
